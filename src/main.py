fdc86726832968e4c9c96578ce69cbb9787ccc6a
Christian Lopez <90930713+chrisprodev@users.noreply.github.com>
2023-04-25 15:13:57 -0600

    Add Transform

--- src/main.py
+++ src/main.py
@@ -20,16 +20,29 @@ def extract():
 
                 df = pd.read_excel(filename_path)
                 transform(df, filename_clean)
-                
-        return
-    except Exception as error:
-        print('Something goes wrong: '+ str(error))
+            
+    except Exception as ex:
+        print('Something went wrong extracting: ' + str(ex))
 
 
 # Transform
 def transform(df, filename):
-    return
-
-
+    try:            
+        print(f'Transforming: {filename}')
+        df_clean = df[['Segment', 'Country', 'Product', 'Gross Sales', 'Discounts',
+        ' Sales', 'COGS', 'Profit', 'Date']].copy()
+        countries = ['United States of America', 'Mexico', 'Canada']
+        df_clean = df_clean[df_clean['Country'].isin(countries)]
+        load(df_clean, filename)
+    except Exception as ex:
+        print('Something went wrong transforming: ' + str(ex))
 
 # Load
+def load(df, filename):
+    try:
+        print(f'Loading: {filename}')
+        print(df)
+    except Exception as ex:
+        print('Loading fail, filename: ' + str(ex))
+
+extract()
